80cd8dd063fb3fd73d1fdefc698424e328e6d5da
Aadee <aadeetyapatil@gmail.com>
2025-03-01 17:45:23 -0500

    initial

---
+++ src/app/auctions/[address]/page.tsx
@@ -0,0 +1,14 @@
+export default function Auction() {
+    const nftData = {
+        title: "Doodle #5221",
+        image: "https://i.seadn.io/s/raw/files/16fa6d078b38ae323e22564171d82e7d.png?auto=format&dpr=1&w=3840",
+        description:
+            "A community-driven collectibles project featuring art by Burnt Toast. Doodles come in a joyful range of colors, traits and sizes with a collection size of 10,000. Each Doodle allows its owner to vote for experiences and activations paid for by the Doodles Community Treasury. Burnt Toast is the working alias for Scott Martin, a Canadian-based illustrator, designer, animator and muralist.",
+        price: 1000,
+        ends: new Date("2025-03-07"),
+        address: "0x1234567890abcdef",
+        creator: "0x1234567890abcdef",
+    };
+
+    return <div>This Auction</div>;
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import Link from "next/link";
+import { ConnectKitButton } from "connectkit";
+import { Button } from "./ui/button";
+
+export function Navbar() {
+    return (
+        <header className="sticky left-0 top-0 z-[100] flex w-full flex-col border-b border-border bg-background">
+            <nav className="flex h-[48px] bg-background justify-center">
+                <div className="container flex items-center justify-between w-full">
+                    <Link href="/" className="font-bold text-3xl">
+                        ArtBlock
+                    </Link>
+
+                    <ConnectKitButton.Custom>
+                        {({
+                            isConnected,
+                            isConnecting,
+                            show,
+                            truncatedAddress,
+                        }) => (
+                            <Button
+                                onClick={show}
+                                className="hover:cursor-pointer"
+                                disabled={isConnecting}
+                                variant={isConnected ? "secondary" : "default"}
+                            >
+                                {isConnected
+                                    ? truncatedAddress
+                                    : isConnecting
+                                    ? "Connecting..."
+                                    : "Connect Wallet"}
+                            </Button>
+                        )}
+                    </ConnectKitButton.Custom>
+                </div>
+            </nav>
+        </header>
+    );
+}
